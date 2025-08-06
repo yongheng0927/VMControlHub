457643971cb9eca20e9d8fff829115b040bc48f1
yongheng.jing <yongh223@outlook.com>
2025-08-06 20:44:58 +0800

    docs(run.py): 添加生产环境调试模式关闭的注释

--- run.py
+++ run.py
@@ -5,4 +5,4 @@ from app import create_app
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=False)
+    app.run(host='0.0.0.0', port=5000, debug=False)  # 生产环境下关闭调试模式
